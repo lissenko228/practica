192c7e5112607b89e7b7fa89421befc6fbedd2f5
lissenko228 <terrariap417@gmail.com>
2023-05-29 17:02:12 +0400

    операции с друзьями

---
+++ task_9/app/Http/Controllers/FriendController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class FriendController extends Controller
+{
+    // мои друзья
+    public function getIndex()
+    {
+        return view('friends.index' , [
+            'friends' => Auth::user()->friends(),
+            'requests' => Auth::user()->friendRequests()
+        ]);
+    }
+
+    public function getAdd($username)
+    {
+        $user = User::where('username', $username)->first();
+
+        if(!$user)
+        {
+            return redirect()->route('home')->with('info', 'Пользователь не найден');
+        }
+
+        if(Auth::user()->id===$user->id)
+        {
+            return redirect()->route('home');
+        }
+
+        if(Auth::user()->hasfriendRequestsPending($user) || $user->hasfriendRequestsPending(Auth::user()))
+        {
+            return redirect()->route('profile.index', ['username' => $user->username])->with('info', 'Пользователю отправлен запрос в друзья');
+        }
+
+        if(Auth::user()->isFriendWith($user))
+        {
+            return redirect()->route('profile.index', ['username' => $user->username])->with('info', 'Пользователь уже в друзьях');
+        }
+
+        Auth::user()->addFriend($user);
+
+        return redirect()->route('profile.index', ['username'=>$username])->with('info', 'Пользователю отправлена заявка в друзья');
+    }
+
+    public function getAccept($username)
+    {
+        $user = User::where('username', $username)->first();
+
+        if(!$user)
+        {
+            return redirect()->route('home')->with('info', 'Пользователь не найден');
+        }
+
+        if(!Auth::user()->hasfriendRequestsReceived($user))
+        {
+            return redirect()->route('home');
+        }
+
+        Auth::user()->acceptFriendRequests($user);
+
+        return redirect()->route('profile.index', ['username' => $username])->with('info', 'Запрос в друзья принят');
+    }
+
+}
