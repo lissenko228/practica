02a163eca4d8f29e3b683e8b59b3692e9bdf27a7
lissenko228 <terrariap417@gmail.com>
2023-05-29 17:01:27 +0400

    добавлен редирект

--- task_9/app/Http/Controllers/HomeController.php
+++ task_9/app/Http/Controllers/HomeController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     function index()
     {
+        if(Auth::check())
+        {
+            return view('timeline.index');
+        }
+
         return view('home');
     }
 }
