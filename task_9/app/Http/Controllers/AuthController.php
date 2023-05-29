35806a7d939d1513202225f4db2a654179a81f26
lissenko228 <terrariap417@gmail.com>
2023-05-29 17:00:58 +0400

    исправление авторизации

--- task_9/app/Http/Controllers/AuthController.php
+++ task_9/app/Http/Controllers/AuthController.php
@@ -50,4 +50,11 @@ class AuthController extends Controller
         return redirect()->route('home')->with('info', 'Вы успешно авторизовались');
             
     }
+
+    public function getSignout()
+    {
+        Auth::logout();
+        
+        return redirect()->route('home');
+    }
 }
