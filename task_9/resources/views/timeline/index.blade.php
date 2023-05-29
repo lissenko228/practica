0457c6594240c06b3deeb2e5831c88de59ada85e
lissenko228 <terrariap417@gmail.com>
2023-05-29 16:53:53 +0400

    стена

---
+++ task_9/resources/views/timeline/index.blade.php
@@ -0,0 +1,19 @@
+@extends('templates/default')
+
+@section('container')
+<div class="row">
+    <div class="col-lg-6">
+        <form action="#" method="post">
+            <div class="form-group mb-2">
+                <textarea class="form-control" name="" placeholder="Что нового" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-dark">Опубликовать</button>
+        </form>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-5">
+        
+    </div>
+</div>
+@endsection
