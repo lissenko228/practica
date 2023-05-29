204ed8fca36aa09944abbed485a056cd1042bd32
lissenko228 <terrariap417@gmail.com>
2023-05-29 17:08:22 +0400

    добавлены роуты

--- task_9/routes/web.php
+++ task_9/routes/web.php
@@ -17,8 +17,26 @@ Route::get('/', 'HomeController@index')->name('home');
 
 // авторизация
 
-Route::get('/signup', 'AuthController@getSignUp')->name('auth.signup');
-Route::post('/signup', 'AuthController@postSignUp');
+Route::get('/signup', 'AuthController@getSignUp')->middleware('guest')->name('auth.signup');
+Route::post('/signup', 'AuthController@postSignUp')->middleware('guest');
 
-Route::get('/signin', 'AuthController@getSignIn')->name('auth.signin');
-Route::post('/signin', 'AuthController@postSignIn');
+Route::get('/signin', 'AuthController@getSignIn')->middleware('guest')->name('auth.signin');
+Route::post('/signin', 'AuthController@postSignIn')->middleware('guest');
+
+Route::get('/signout', 'AuthController@getSignout')->name('auth.signout');
+
+// поиск
+
+Route::get('/search', 'SearchController@getResults')->name('search.results');
+
+// Профили
+
+Route::get('/user/{username}', 'ProfileController@getProfile')->name('profile.index');
+Route::get('/profile/edit', 'ProfileController@getEdit')->middleware('auth')->name('profile.edit');
+Route::post('/profile/edit', 'ProfileController@postEdit')->middleware('auth')->name('profile.edit');
+
+// друзья
+
+Route::get('/friends', 'FriendController@getIndex')->middleware('auth')->name('friend.index');
+Route::get('/friends/add/{username}', 'FriendController@getAdd')->middleware('auth')->name('friend.add');
+Route::get('/friends/accept/{username}', 'FriendController@getAccept')->middleware('auth')->name('friend.accept');
