4061031e9274f0091142e7c76202639b18441110
lissenko228 <terrariap417@gmail.com>
2023-05-30 16:50:00 +0400

    редиректы

--- task_9/app/Http/Controllers/HomeController.php
+++ task_9/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Auth;
+use App\Models\Status;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,7 +12,14 @@ class HomeController extends Controller
     {
         if(Auth::check())
         {
-            return view('timeline.index');
+            $statuses=Status::notReply()->where
+            (function($query)
+                {
+                    return $query->where('user_id', Auth::user()->id)->orWhereIn('user_id', Auth::user()->friends()->pluck('id'));
+                }
+            )->orderBy('created_at', 'desc')->paginate(10);
+
+            return view('timeline.index', compact('statuses'));
         }
 
         return view('home');
